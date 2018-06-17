31dc884974a16f2ce4cbbfbffbdfa8398c828da0
Jaromir Sys <jarda.sys@gmail.com>
2018-06-18 00:02:00 +0200

    PiLiS apply response from PiReC to lights

--- PiLiS/src/main/java/technology/sys/pilis/plugins/LightControllerPlugin.java
+++ PiLiS/src/main/java/technology/sys/pilis/plugins/LightControllerPlugin.java
@@ -2,10 +2,13 @@ package technology.sys.pilis.plugins;
 
 import java.io.IOException;
 import java.net.Socket;
+
+import org.apache.commons.lang3.BooleanUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Service;
+
 import technology.sys.pilis.comon.Action;
 import technology.sys.pilis.comon.GpioRequest;
 import technology.sys.pilis.comon.GpioResponse;
@@ -20,60 +23,65 @@ import technology.sys.pilis.interfaces.ILightControllerPlugin;
  */
 @Service
 @ComponentScan(value = "technology.sys.pilis.plugins")
-public class LightControllerPlugin implements ILightControllerPlugin
-{
+public class LightControllerPlugin implements ILightControllerPlugin {
 
-    private static final Logger LOG = LogManager.getLogger(LightControllerPlugin.class);
+	private static final Logger LOG = LogManager
+			.getLogger(LightControllerPlugin.class);
 
-    @Override
-    public void turnOn(Light light)
-    {
-        GpioRequest request = new GpioRequest(Action.TURN_ON, light.getPinName());
-        GpioResponse response = sendRequest(request, light.getControllerIp());
-        if (response != null)
-        {
-            light.turnOn();
-        }
+	@Override
+	public void turnOn(Light light) {
+		GpioRequest request = new GpioRequest(Action.TURN_ON,
+				light.getPinName());
+		GpioResponse response = sendRequest(request, light.getControllerIp());
+		if (response != null) {
+			applyStateFromReponse(response, light);
+		}
 
-    }
+	}
 
-    @Override
-    public void turnOff(Light light)
-    {
-        GpioRequest request = new GpioRequest(Action.TURN_OFF, light.getPinName());
-        GpioResponse response = sendRequest(request, light.getControllerIp());
-        if (response != null)
-        {
-            light.turnOff();
-        }
-    }
+	@Override
+	public void turnOff(Light light) {
+		GpioRequest request = new GpioRequest(Action.TURN_OFF,
+				light.getPinName());
+		GpioResponse response = sendRequest(request, light.getControllerIp());
+		if (response != null) {
+			applyStateFromReponse(response, light);
+		}
+	}
 
-    @Override
-    public void toggle(Light light)
-    {
-        GpioRequest request = new GpioRequest(Action.TOGGLE, light.getPinName());
-        GpioResponse response = sendRequest(request, light.getControllerIp());
-        if (response != null)
-        {
-            light.toggle();
-        }
-    }
+	@Override
+	public void toggle(Light light) {
+		GpioRequest request = new GpioRequest(Action.TOGGLE,
+				light.getPinName());
+		GpioResponse response = sendRequest(request, light.getControllerIp());
+		if (response != null) {
+			applyStateFromReponse(response, light);
+		}
+	}
 
-    private GpioResponse sendRequest(GpioRequest request, String controllerIp)
-    {
-        try (Socket socket = new Socket(controllerIp, 50000))
-        {
-            String requestString = XmlHelper.marshallRequest(request);
-            SocketHelper.sendString(socket, requestString);
+	private GpioResponse sendRequest(GpioRequest request, String controllerIp) {
+		try (Socket socket = new Socket(controllerIp, 50000)) {
+			String requestString = XmlHelper.marshallRequest(request);
+			SocketHelper.sendString(socket, requestString);
 
-            String responseString = SocketHelper.readAllFromSocket(socket);
-            GpioResponse response = XmlHelper.unmarshallResponse(responseString);
-            return response;
-        }
-        catch (IOException ex)
-        {
-            LOG.error(ex);
-            return null;
-        }
-    }
+			String responseString = SocketHelper.readAllFromSocket(socket);
+			GpioResponse response = XmlHelper
+					.unmarshallResponse(responseString);
+			return response;
+		} catch (IOException ex) {
+			LOG.error(ex);
+			return null;
+		}
+	}
+
+	private void applyStateFromReponse(GpioResponse response, Light light) {
+		Boolean stateOfLight = response.getPinMap().get(light.getPinName());
+		if (stateOfLight == null) {
+			light.stateUnknow();
+		} else if (BooleanUtils.isTrue(stateOfLight)) {
+			light.turnOn();
+		} else {
+			light.turnOff();
+		}
+	}
 }
